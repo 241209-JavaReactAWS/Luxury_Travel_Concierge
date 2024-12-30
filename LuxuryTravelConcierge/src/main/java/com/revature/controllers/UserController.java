9f2fcc1a5fb395898ef0e800443d8c1b2415bbe6
Ritik Patel <ritikp23@gmail.com>
2024-12-30 11:40:34 -0500

    created user controller and user login method

---
+++ LuxuryTravelConcierge/src/main/java/com/revature/controllers/UserController.java
@@ -0,0 +1,56 @@
+package com.revature.controllers;
+
+import com.revature.exceptions.*;
+import com.revature.models.User;
+import com.revature.services.UserSevice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+    private final UserSevice userSevice;
+
+    @Autowired
+    public UserController(UserSevice userSevice) {
+        this.userSevice = userSevice;
+    }
+
+
+    @PostMapping
+    public ResponseEntity registerUserHandler(@RequestBody User user){
+        try{
+            User newUser = userSevice.registerUser(user);
+            return ResponseEntity.status(HttpStatus.OK).body(newUser);
+        }
+        catch(UsernameException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username is invalid");
+        }
+        catch (UsernameExistsException e){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Username is taken");
+        }
+        catch (PasswordException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Password is invalid");
+        }
+        catch(NoEmailException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email is invalid");
+        }
+        catch(NoAddressException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Address is invalid");
+        }
+        catch(NoFirstNameException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No first name");
+        }
+        catch(NoLastNameException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No last name");
+        }
+        catch(Exception e){
+            return ResponseEntity.status(HttpStatus.SEE_OTHER).body(null);
+        }
+    }
+}
--- LuxuryTravelConcierge/src/main/java/com/revature/services/UserSevice.java
+++ LuxuryTravelConcierge/src/main/java/com/revature/services/UserSevice.java
@@ -1,9 +1,12 @@
 package com.revature.services;
 
 import com.revature.DAOS.UserDAO;
+import com.revature.exceptions.*;
 import com.revature.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserSevice {
@@ -14,4 +17,37 @@ public class UserSevice {
         this.userDAO = userDAO;
     }
 
+    public Optional<User> findUserById(int userId){
+        return userDAO.findById(userId);
+    }
+
+    public Optional<User> findUserByUsername(String username){
+        return userDAO.findUserbyUsername(username);
+    }
+
+    // register user
+    public User registerUser(User user) throws PasswordException, UsernameException, UsernameExistsException, NoEmailException, NoAddressException, NoFirstNameException, NoLastNameException {
+        if(user.getPassword().length() < 5){
+            throw new PasswordException();
+        }
+        if(user.getUsername().trim().isEmpty()){
+            throw new UsernameException();
+        }
+        if(userDAO.findUserbyUsername(user.getUsername()).isPresent()){
+            throw new UsernameExistsException();
+        }
+        if(user.getEmail().isEmpty()){
+            throw new NoEmailException();
+        }
+        if(user.getAddress().isEmpty()){
+            throw new NoAddressException();
+        }
+        if(user.getFirst_name().isEmpty()){
+            throw new NoFirstNameException();
+        }
+        if(user.getLast_name().isEmpty()){
+            throw new NoLastNameException();
+        }
+        return userDAO.save(user);
+    }
 }
