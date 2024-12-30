acc972311ae7640deaebd704613fc1853b97f335
Ritik Patel <ritikp23@gmail.com>
2024-12-30 09:25:06 -0500

    added backend files

---
+++ LuxuryTravelConcierge/src/main/java/com/revature/LuxuryTravelConcierge/LuxuryTravelConciergeApplication.java
@@ -0,0 +1,13 @@
+package com.revature.LuxuryTravelConcierge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxuryTravelConciergeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuxuryTravelConciergeApplication.class, args);
+	}
+
+}
---
+++ LuxuryTravelConcierge/src/test/java/com/revature/LuxuryTravelConcierge/LuxuryTravelConciergeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.LuxuryTravelConcierge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuxuryTravelConciergeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
