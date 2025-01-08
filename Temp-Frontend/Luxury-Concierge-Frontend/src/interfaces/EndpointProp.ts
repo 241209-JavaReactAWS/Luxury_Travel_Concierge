1bd57ed24c2b4be2bee1ecc462beb083c7527dcb
mattm <kumkowskimateusz2002@gmail.com>
2025-01-08 10:15:51 -0500

    Changed Some things

--- Temp-Frontend/Luxury-Concierge-Frontend/src/interfaces/EndpointProp.ts
+++ Temp-Frontend/Luxury-Concierge-Frontend/src/interfaces/EndpointProp.ts
@@ -1,6 +1,12 @@
 import onError from "./onErrorInterface"
 import onSuccess from "./onSuccessInterface"
 
+/*
+    This is for sending and endpoint as props for a component
+    onSuccess is for the axios.then
+    onError is for the axios.catch
+*/
+
 interface EndpointProp {
     placeholder ?: string,
     endpoint : string,
