705abdbae0ee42f57dbcd53794736e20eeaebfa6
sharvanitamaskar <sharvanitamaskar@gmail.com>
2025-01-13 10:56:21 -0500

    update

--- Temp-Frontend/Luxury-Concierge-Frontend/src/components/RoomComponent/Rooms.tsx
+++ Temp-Frontend/Luxury-Concierge-Frontend/src/components/RoomComponent/Rooms.tsx
@@ -169,13 +169,11 @@ return (
                         />
                         <Box sx={{ display: 'flex', flexDirection: 'column', padding: '10px' }}>
                             <CardContent sx={{ flex: '1 0 auto' }}>
-                                <Typography component="div" variant="h5">
+                                {/* <Typography component="div" variant="h5">
                                     {room.roomName}
-                                </Typography>
+                                </Typography> */}
                                 <Typography
-                                    variant="subtitle1"
-                                    component="div"
-                                    sx={{ color: 'text.secondary' }}
+                                    component="div" variant="h5"
                                 >
                                     Type: {room.roomType}
                                 </Typography>
